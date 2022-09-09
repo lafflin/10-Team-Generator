bdade6cd8796d7c550dc9f9f746b771cdc2afbb0
lafflin <maxwelljmack@gmail.com>
2022-09-08 18:23:56 -0700

    added some test stuff as well as the ability to use inquirer to make a new manager

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,21 @@
-//  tests need to validate the existence of each class INDIVIDUALLY and that they work
+const Employee = require("../lib/Employee");
+
+test("Can set name via constructor", () => {
+	const name = "Max";
+	const a = new Employee(name);
+	expect(a.name).toBe(name);
+});
+
+test("Can set id via constructor", () => {
+	const id = "7";
+	const b = new Employee("Max", id);
+	expect(b.id).toBe(id);
+});
+
+test("Can set email via constructor", () => {
+	const email = "test@gmail.com";
+	const c = new Employee("Max", 7, email);
+	expect(c.email).toBe(email);
+});
+
+// add tests for getname, getId, getEmail, and getRole
--- index.js
+++ index.js
@@ -3,10 +3,47 @@ const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern.js");
 const Engineer = require("./lib/Engineer.js");
 const Manager = require("./lib/Manager.js");
-const Inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-// write the inquirer stuff in here
+// empty array to store the created employees
+let createdEmployees = [];
+
+// inquirer prompt for manager info
+inquirer
+	.prompt([
+		{
+			message: "What is the name of the manager?",
+			type: "input",
+			name: "name",
+		},
+		{
+			message: "What is the managers employee Id?",
+			type: "input",
+			name: "id",
+		},
+		{
+			message: "What is the managers email?",
+			type: "input",
+			name: "email",
+		},
+		{
+			message: "What is the managers office number?",
+			type: "input",
+			name: "officeNumber",
+		},
+	])
+	// takes the stuff from the questions, creates a new manager with the info
+	.then((manager) => {
+		let newManager = new Manager(
+			manager.name,
+			manager.id,
+			manager.email,
+			manager.officeNumber
+		);
+		// then pushes the new manager to the empty array
+		createdEmployees.push(newManager);
+	});
 
 // also need a sample of the HTML doc created in dist folder
