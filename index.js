f62f378157601ac0b9c6bf0b950c038969b1dc7c
lafflin <maxwelljmack@gmail.com>
2022-09-08 17:34:00 -0700

    added all of the classes and required the modules in the index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// all of the requires
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern.js");
+const Engineer = require("./lib/Engineer.js");
+const Manager = require("./lib/Manager.js");
+const Inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
 // write the inquirer stuff in here
 
 // also need a sample of the HTML doc created in dist folder
